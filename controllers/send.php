4950b440fb782515c0f24a90afb540f0ea7e3612
CristiansSans <cristiansleonardo@gmail.com>
2020-08-07 17:06:20 -0400

    changes

--- controllers/send.php
+++ controllers/send.php
@@ -140,7 +140,7 @@ class Mails {
         $origenNombre = 'Información de cliente';
         $origenEmail = 'kkprettynails@gmail.com';
         $destinatarioEmail = "kkprettynails@gmail.com"; 
-
+        $destinatarioEmailTwo = $correo;
         //cuerpo del email:
         $cuerpoMensaje = "Se ha recibido una solicitud de contacto con los siguientes datos:
         ";
@@ -148,7 +148,48 @@ class Mails {
         $cuerpoMensaje .= "Correo:".$correo."<br>";
         $cuerpoMensaje .= "Mensaje:\r\n".$mensaje."<br>";
         //fin cuerpo del email.
+        $cuerpoMensajeDos = '
+        <div style="width: 100%; padding:0;text-align:center;">
+        <div style="width: 85%;height: 8vh;margin: auto;background-color: #fdd3d7;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;text-align:justify;-webkit-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);-moz-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);">
+            <div style="width: 100px;margin:auto;border-radius:55%;background-color:#f8f9fa;padding: 10px;">     
+                <img style="width: 100%;" src="http://kkprettynails.cl/views/images/logokk.png" alt="Logo kkprettynails">
+            </div>
+        </div>
+        <div style="width: 100%;margin: auto;padding-top: 5%;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;padding-bottom: 40px;">
+            <center>
+                <div style="width:60%;text-align: center;">
+                    
+                    <p style="text-align:center;margin-top:10px;font-size:30px;"> <strong>Hola '.$nombre.'.</p>
+                    <p style="text-align:left;font-size:16px;font-weight: 300;margin:auto;"><strong> 
+                    Hemos recibido tu solicitud de contacto, a la brevedad nos comunicaremos para responder tus dudas o
+                    consultas.
+                    </strong> <br>
+                    Detalle del mensaje: '.$mensaje.'
 
+                    </p>
+                <div>
+            </center>
+        </div>
+        <div style="width: 85%;background-color: #f0f1f3;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);margin: auto;padding: 20px;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;padding-bottom:20px;-webkit-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);-moz-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);">
+            <center>
+            <div style="width:60%;">
+                <center>
+                
+                <p style="text-align:center;font-size:18px;">Contáctanos.</p>
+                <a href="mailto:kkprettynails@gmail.com"><img style="width: 45px;" src="http://kkprettynails.cl/views/images/mail.png" alt=""></a>
+                <a href="https://www.instagram.com/kkprettynails/?hl=es-la"><img style="width: 40px;margin-left:40px" src="http://kkprettynails.cl/views/images/ig.png" alt=""></a>
+                <a href="https://wa.me/56972628949"><img style="width:38px;margin-left:40px" src="http://kkprettynails.cl/views/images/ws.png" alt=""></a>
+                <a href="https://kkprettynails.cl"><img style="width: 40px;margin-left:40px" src="http://kkprettynails.cl/views/images/web.png" alt=""></a>
+                <br>
+                <a href="https://goo.gl/maps/GhvcDBH1ppBDae1KA">
+                    <p>Av. Pedro de Valdivia 3474, Ñuñoa, Región Metropolitana de Santiago</p>
+                   </a>
+                </center>
+            </div>
+            </center>
+        </div>
+    </div>
+        ';
         $mail = new PHPMailer(true);
 
         try {
@@ -172,6 +213,34 @@ class Mails {
             $mail->Body    = $cuerpoMensaje;
 
             $mail->send();
+            $mailTwo = new PHPMailer(true);
+
+                try {
+                    //Server settings
+                    $mailTwo->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+                    $mailTwo->isSMTP();                                            // Send using SMTP
+                    $mailTwo->SMTPAuth   = true;                                   // Enable SMTP authentication
+                    $mailTwo->Username   = 'kkprettynails@gmail.com';                     // SMTP username
+                    $mailTwo->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+                    $mailTwo->Password   = $passSend;                               // SMTP password
+                    $mailTwo->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                    $mailTwo->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+                    //Recipients
+                    $mailTwo->setFrom($origenEmail, 'Contacto KKPrettynails');
+                  
+                    $mailTwo->addAddress($destinatarioEmailTwo, $nombre);     // Add a recipient
+
+                    // Content
+                    $mailTwo->isHTML(true);                      // Set email format to HTML
+                    $mailTwo->Subject = 'Contacto KKPrettynails';
+                    $mailTwo->Body    = $cuerpoMensajeDos;
+                    
+                    $mailTwo->send();
+                    
+                } catch (Exception $e) {
+                    echo $e;
+                }
             echo '
             <script>
                 document.querySelector("body").style.color = "white";
@@ -417,11 +486,27 @@ public function giftMail()
 
         $correo = $_SESSION["correo"];
         $nombre = $_SESSION["nombre"];
+        $numero = $_SESSION["numero"];
         $apellido = $_SESSION["apellido"];
         $articulo = $_POST['articulogift'];
         $total = $_POST['totalGift'];
         $metodo = $_POST['typePay'];
-            
+        $date = date("d-m-Y");
+        if ($metodo == "Transferencia") {
+            $datosMetodo = "Este pedido no será procesado hasta que sea transferido el total del pago y envíes la copia del comprobante a la dirección de correo indicada más abajo.";
+            $metodoInfo = '<center>   
+            <div style="padding: 10px;border-radius: 5px;width: 60%;text-align: left;background-color: #fdd3d7;margin-top: 10px;">
+            <span style="font-weight: bold">Datos bancarios para la transferencia:</span>
+            <p>Nombre: <br> RUT: <br> Cuenta: <br> Banco: <br> Email: kkprettynails@gmail.com <br> Comentario: *Por favor indicar el código del pedido*</p>
+            </div>
+        </center>';
+        }
+        else{
+            $datosMetodo = "Este código tiene fecha de vencimiento de 7 días hábiles a pagar en la
+            dirección correspondiente en la siguiente dirección: Av. Pedro de Valdivia 3474, local 53B, Ñuñoa,
+            Región Metropolitana de Santiago.";
+            $metodoInfo = '';
+        }    
             echo  ' <script>document.querySelector("body").style.color = "white"</script>';
             $origenNombre = 'Compra de gift card';
             $origenEmail = 'kkprettynails@gmail.com';
@@ -430,46 +515,80 @@ public function giftMail()
             //cuerpo del email:
             $cuerpoMensaje = '
             <div style="width: 100%; padding:0;text-align:center;">
-            <div style="width: 85%;height: 8vh;margin: auto;background-color: #fdd3d7;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;text-align:justify;-webkit-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);-moz-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);">
-                <div style="width: 100px;margin:auto;border-radius:55%;background-color:#f8f9fa;padding: 10px;">     
-                    <img style="width: 100%;" src="http://kkprettynails.cl/views/images/logokk.png" alt="Logo kkprettynails">
-                </div>
-            </div>
-            <div style="width: 100%;margin: auto;padding-top: 5%;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;padding-bottom: 40px;">
-                <center>
-                    <div style="width:60%;text-align: center;">
-                        <h1 style="text-align: center;color:#172b4d;">Bienvenid@ </h1>
-                        <p style="text-align:center;margin-top:10px;font-size:18px;"> <strong>Estimado(a) '.$nombre.' '.$apellido.'. Usted realizo una compra por la Gift Card: '.$articulo.' y su metodo de pago es: '.$metodo.' </p>
-                        <p style="text-align:left;font-size:14px;font-weight: 300;text-align: center;width: 60%;margin:auto;"><strong> 
-                        Estos son los datos de tu compra y los pasos para realizar el pago:  </strong>
-                        </p>
-
-                        
-
-                        <p style="text-align:left;font-size:14px;font-weight: 300;text-align: center;width: 60%;margin:auto;"><strong> <br>
-                         Si usted no realizó esta acción, ignore este correo. </strong>
-                        </p>
-                    <div>
-                </center>
-            </div>
-            <div style="width: 85%;background-color: #f0f1f3;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);margin: auto;padding: 20px;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;padding-bottom:20px;-webkit-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);-moz-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);">
-                <center>
-                <div style="width:60%;">
-                    <center>
-                        <p style="text-align:center;font-size:18px;">Contáctanos.</p>
-                        <a href="mailto:kkprettynails@gmail.com"><img style="width: 45px;" src="http://kkprettynails.cl/views/images/mail.png" alt=""></a>
-                        <a href="https://www.instagram.com/kkprettynails/?hl=es-la"><img style="width: 40px;margin-left:40px" src="http://kkprettynails.cl/views/images/ig.png" alt=""></a>
-                        <a href="https://wa.me/56972628949"><img style="width:38px;margin-left:40px" src="http://kkprettynails.cl/views/images/ws.png" alt=""></a>
-                        <a href="https://kkprettynails.cl"><img style="width: 40px;margin-left:40px" src="http://kkprettynails.cl/views/images/web.png" alt=""></a>
-                        <br>
-                        <a href="https://goo.gl/maps/GhvcDBH1ppBDae1KA">
-                    <p>Av. Pedro de Valdivia 3474, Ñuñoa, Región Metropolitana de Santiago</p>
-                   </a>
-                    </center>
-                </div>
-                </center>
+        <div style="width: 85%;height: 8vh;margin: auto;background-color: #fdd3d7;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);padding: 20px;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;text-align:justify;-webkit-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);-moz-box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);box-shadow: 0px 6px 8px -8px rgba(0,0,0,0.73);">
+            <div style="width: 100px;margin:auto;border-radius:55%;background-color:#f8f9fa;padding: 10px;">     
+                <img style="width: 100%;" src="http://kkprettynails.cl/views/images/logokk.png" alt="Logo kkprettynails">
             </div>
         </div>
+        <div style="width: 100%;padding-top: 5%;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;padding-bottom: 40px;">
+            <center>
+                <div style="width:60%;text-align: left;border-bottom: 3px solid #fdd3d7;">
+                    <p style="text-align:left;margin-top:10px;font-size:18px;"> Estimado(a) '.$nombre.' '.$apellido.', <br> Hemos recibido una solicitud de compra por pagar en '.$metodo.'. Para realizar este pago debes
+                        presentar en caja el siguiente código: 1. </p>
+                    </div>
+                <div style="width:60%;text-align: left;border-bottom: 3px solid #fdd3d7;"> <br>
+                    <p style="text-align:left;margin-top:10px;font-size:18px;"> <span style="font-weight: bold">Información importante:</span>  '.$datosMetodo.'
+                         </p>
+                </div>
+                '.$metodoInfo.'
+                <div style="width:60%;text-align: left;border-bottom: 3px solid #fdd3d7;"> <br>
+                    <p style="text-align:left;margin-top:10px;font-size:18px;"> <span style="font-weight: bold">A continuación te dejamos os detalles de tu compra: <br><br>
+                        Nombre: '.$nombre.' '.$apellido.' <br>
+                        Monto: '.$total.'<br>
+                        Código de compra: 1 <br>
+                        Teléfono: '.$numero.' <br>
+                        Email: '.$correo.' <br>
+                        Fecha de compra: '.$date.' <br>
+                         </p>
+                </div>
+
+                <table style="padding: 10px;">
+                    <tr style="border-bottom: 3px solid #fdd3d7">
+                      <th style="padding: 10px;text-align: center;">ID</th>
+                      <th style="padding: 10px;text-align: center;">Producto</th>
+                      <th style="padding: 10px;text-align: center;">Compra</th>
+                      <th style="padding: 10px;text-align: center;">Valor</th>
+                      <th style="padding: 10px;text-align: center;">Cantidad</th>
+                      <th style="padding: 10px;text-align: center;">Subtotal</th>
+                    </tr>
+                    <tr>
+                      <td style="padding: 10px;text-align: center;">1</td>
+                      <td style="padding: 10px;text-align: center;">Gift Card</td>
+                      <td style="padding: 10px;text-align: center;">'.$articulo.'</td>
+                      <td style="padding: 10px;text-align: center;">'.$total.'</td>
+                      <td style="padding: 10px;text-align: center;">01</td>
+                      <td style="padding: 10px;text-align: center;">'.$total.'</td>
+                    </tr>
+                   
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td style="padding: 10px;text-align: center;">Total:</td>
+                      <td style="padding: 10px;text-align: center;">'.$total.'</td>
+                    </tr>
+                  </table>
+            </center>
+        </div>
+        <div style="width: 85%;background-color: #f0f1f3;box-shadow: 0 2px 5px 0 rgba(0,0,0,.14);margin: auto;padding: 20px;font-family: Roboto,RobotoDraft,Helvetica,Arial,sans-serif;color:#172b4d;padding-bottom:20px;-webkit-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);-moz-box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);box-shadow: 0px -4px 11px 0px rgba(0,0,0,0.12);">
+            <center>
+            <div style="width:60%;">
+                <center>
+                    <p style="text-align:center;font-size:18px;">Contáctanos.</p>
+                    <a href="mailto:kkprettynails@gmail.com"><img style="width: 45px;" src="http://kkprettynails.cl/views/images/mail.png" alt=""></a>
+                    <a href="https://www.instagram.com/kkprettynails/?hl=es-la"><img style="width: 40px;margin-left:40px" src="http://kkprettynails.cl/views/images/ig.png" alt=""></a>
+                    <a href="https://wa.me/56972628949"><img style="width:38px;margin-left:40px" src="http://kkprettynails.cl/views/images/ws.png" alt=""></a>
+                    <a href="https://kkprettynails.cl"><img style="width: 40px;margin-left:40px" src="http://kkprettynails.cl/views/images/web.png" alt=""></a>
+                    <br>
+                   <a href="https://goo.gl/maps/GhvcDBH1ppBDae1KA">
+                    <p>Av. Pedro de Valdivia 3474, Ñuñoa, Región Metropolitana de Santiago</p>
+                   </a>
+                </center>
+            </div>
+            </center>
+        </div>
+    </div>
             ';
            
         
