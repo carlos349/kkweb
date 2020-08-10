d90c211fdaa03851697823e63d717550d9c70609
CristiansSans <cristiansleonardo@gmail.com>
2020-08-10 14:40:17 -0400

    changes

--- views/template.php
+++ views/template.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="description" content="Un spa hecho a tu medida ♥">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="https://fonts.googleapis.com/css2?family=Kavivanar&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="views/images/logokk.png" type="image/x-icon">
     <link rel="icon" href="views/images/logokk.png" type="image/x-icon">
